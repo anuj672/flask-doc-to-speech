77ce7ed6cc2cc5c92523ba89e1e4c4edff4d91f8
achetwa <anujchetwani@gmail.com>
2024-11-30 20:03:53 -0500

    post PDF (postman)

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,9 @@ import os
 
 route_polly = Blueprint('route_polly', __name__)
 
-@route_polly.route('/')
+@route_polly.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        file = request.files.get('file')
+        return "got it"   
     return "Welcome!"
